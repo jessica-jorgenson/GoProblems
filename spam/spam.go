4431bd31ffd6a56c0a711f0c84c98072971da145
Jessica Jorgenson <jorgenson.jess@gmail.com>
2020-05-27 23:42:04 -0600

    Almost perfected regex, began to write matches to file

--- spam/spam.go
+++ spam/spam.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func main() {
@@ -20,10 +21,37 @@ func main() {
 		return
 	}
 	defer outputFile.Close()
-	text := scanner.Text()
-	for len(text) == 0 {
-		fmt.Println("Yup")
-		scanner.Scan()
+
+	/*//re := regexp.MustCompile(`(?!\.)(?!.*\.$)(?!.*?\.\.)[\w.-]+@(?!\.)(?!.*\.$)(?!.*?\.\.)[\w_.]+`)
+	re := regexp.MustCompile(`@`)
+	for scanner.Scan() {
+		if len(scanner.Text()) > 0 && strings.Contains(scanner.Text(), "@") {
+			matches := re.FindAllStringIndex(scanner.Text(), -1)
+			for _, atLocation := range matches {
+				line := string(scanner.Text())
+				myIndex := int(atLocation[0])
+				beginningPortion := ""
+				for i := myIndex; i >= 0; i -- {
+
+				}
+			}
+		}
+	}*/
+
+	outputFile.WriteString("Start of Spam Program\n")
+
+	re := regexp.MustCompile(`((\w|\d|\-|\_)+(.?))+@((\w|\d|\-|\_)+(.?))+`)
+	for scanner.Scan() {
+		matches := re.FindAllString(scanner.Text(), -1)
+		for _, match := range matches {
+			if len(match) > 0 {
+				outputFile.WriteString(match + "\n")
+			}
+		}
 	}
+
+	outputFile.WriteString("End of Spam Program")
+
+	outputFile.Sync()
 
 }
